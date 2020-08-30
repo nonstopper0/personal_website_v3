c4922928133916a50f8f4f5092b67c34fddce89f
Nathaniel Redmon <52013906+nonstopper0@users.noreply.github.com>
2020-08-30 06:07:41 -0600

    Updates

---
+++ precache-manifest.3ec60e5a8a3d7efc57879e953cdb8852.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da270c82448d87b98b06d3014e250014",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7eaa4547afa513ebff18",
+    "url": "/static/css/main.1087cc08.chunk.css"
+  },
+  {
+    "revision": "8c3029db69c8bdd814c5",
+    "url": "/static/js/2.8faaaec3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8faaaec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eaa4547afa513ebff18",
+    "url": "/static/js/main.8877cd7d.chunk.js"
+  },
+  {
+    "revision": "19ca6ffd5a1f8d34a1be",
+    "url": "/static/js/runtime-main.045f60b1.js"
+  },
+  {
+    "revision": "c8a1f97bd8f59d7806bdae984963816e",
+    "url": "/static/media/Load.c8a1f97b.svg"
+  },
+  {
+    "revision": "5ad4ed36f934397352e5d4b97c50b9f6",
+    "url": "/static/media/Sprite Graffiti Extras.5ad4ed36.otf"
+  },
+  {
+    "revision": "a1e3ab3a706e932d3e2e50b3744ecb3d",
+    "url": "/static/media/Sprite Graffiti.a1e3ab3a.otf"
+  },
+  {
+    "revision": "a2d73de32f84f76881259bb137764bee",
+    "url": "/static/media/can.a2d73de3.svg"
+  },
+  {
+    "revision": "d90c05bb29086a0b1f41e03e666cb269",
+    "url": "/static/media/edge.d90c05bb.svg"
+  },
+  {
+    "revision": "7641086281638022b64939b421baba50",
+    "url": "/static/media/me.76410862.jpg"
+  },
+  {
+    "revision": "467a9f439a5ba9931f0fb817d1838100",
+    "url": "/static/media/navcan.467a9f43.svg"
+  }
+]);
